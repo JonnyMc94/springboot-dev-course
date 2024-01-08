b4df269facdc3d6212d89a3bb6c9cbb74ed05358
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-01-08 18:23:25 +0000

    First commit

---
+++ 01-springboot-overview/02-dev-tools-demo/src/test/java/com/luv2code/spingboot/demo/udemyspringbootapp/UdemyspringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.spingboot.demo.udemyspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyspringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 01-springboot-overview/04-security-demo/src/main/java/com/luv2code/spingboot/demo/udemyspringbootapp/UdemyspringbootappApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.spingboot.demo.udemyspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  UdemyspringbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyspringbootappApplication.class, args);
+	}
+
+}
