25f20da81535670c6d08d8e0fc932a43eb44885d
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-01-13 23:05:45 +0000

    Rest of Section 2

---
+++ 02-spring-boot-springcore/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,18 @@
+package com.luv2code.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    public CricketCoach() {
+        System.out.println("in constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice bowling for 15 mins :)";
+    }
+}
